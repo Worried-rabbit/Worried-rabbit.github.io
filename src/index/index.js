c25de86e6ffa033ff25786a9ec68b76d6d63dbe8
rabbit <1500679598@qq.com>
2021-11-15 08:29:03 +0800

    增加了一个页面

--- src/index/index.js
+++ src/index/index.js
@@ -1,21 +1,5 @@
-import Ani from "../../assets/js/Ani.js"
-import webpackConfig from '../../config/webpack.config.js'
-import Webpack from '../../assets/js/Webpack.js'
-
-let webpack = new Webpack(webpackConfig);
-console.log(webpack);
-
-
-let ani = new Ani({
-    targets: [{
-        el: document.querySelector('.box1'),
-        animationClassName: 'change'
-    }, {
-        el: document.querySelector('.box2'),
-        animationClassName: 'change'
-    }],
-    count: 1,
-    reverse: true
-})
-
-console.log(ani);
+document.querySelector('.end').onclick = function() {
+    let wrap = document.getElementById('wrap');
+    console.log(wrap);
+    wrap.className = 'owrap'
+}
